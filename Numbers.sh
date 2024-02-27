268bf9c5dd5977fd1fd3dedea9d0d3f044ffcff6
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-02-27 09:13:34 +0530

    Create Numbers.sh

---
+++ Numbers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Numbers From 1 to 100"           
+for i in {1..100}                                                               
+do echo $i                                                                      
+done
