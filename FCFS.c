838023a36a16aab9498f84d6c49aa8eb7ec5762c
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-03-19 11:23:58 +0530

    Update FCFS.c
    Perform FCFS without Arrival time

--- FCFS.c
+++ FCFS.c
@@ -1,72 +1,33 @@
 #include<stdio.h>
-typedef struct{
-	int pid,at,bt,wt,tt;
-}Process;
-void TakeInput(Process p[],int n){
-		for(int i=0;i<n;i++){
-			printf("Enter arrival and burst of P%d: ",i);
-			scanf("%d  %d",&p[i].at,&p[i].bt);
-			p[i].pid=i;
-			}
-}
-void swap(Process p[],int i,int j){
-	Process temp;
-	temp=p[i];
-	p[i]=p[j];
-	p[j]=temp;
-}	
-void sort(Process p[],int n){
-	for(int i=1;i<n;i++){
-		for(int j=0;j<n-i;j++){
-			if(p[j].at > p[j+1].at){
-				swap(p,j,j+1);
-			}
-		}
-	}
-}
-void sortPid(Process p[],int n){
-	for(int i=1;i<n;i++){
-		for(int j=0;j<n-i;j++){
-			if(p[j].pid > p[j+1].pid){
-				swap(p,j,j+1);
-			}
-		}
-	}
-}
-void display(Process p[],int n){	
-	float w=0,t=0;
-	printf("-----------------------------------------------------\n");
-	printf("Process \t\t AT \t BT \t WT \t TT\n");
-	for(int i=0;i<n;i++){
-		printf("  %d  \t\t %d \t %d \t %d \t %d\n",i,p[i].at,p[i].bt,p[i].wt,p[i].tt);
-		t+=p[i].tt;
-		w+=p[i].wt;
-		}
-		w=w/n;
-		t=t/n;
-	printf("Average Waiting Time:%f",w);
-	printf("Average Turn Around Time:%f",t);
-}
+struct Process{
+	int bt,wt,tt,pid;
+}p;
 void main(){
-	int n;
-	printf("Enter the number of process: ");
-	scanf("%d",&n);
-	Process p[n];
-	TakeInput(p,n);
-	sort(p,n);
-	/*int t=0;
-	for(int i=0;i<n;i++){
-		if(p[i].at > t){
-			printf("%d| ** |",t);
-			t = p[i].at;
-		}
 
-		printf("%d| P%d |",t,p[i].pid);
-		t = t + p[i].bt;
-		p[i].tt = t-p[i].at;
-		p[i].wt = p[i].tt - p[i].bt;
+	int n,waitingtime,totaltime=0,twait;
+	printf("Enter number of process:");
+	scanf("%d",&n);
+	struct Process p[n];
+	for(int i=0;i<n;i++){
+		printf("Enter burst time of Process%d: ",i);
+		scanf("%d",&p[i].bt);
+		p[i].pid=i+1;
+		}
+	for(int i=0;i<n;i++){
+		p[i].wt=totaltime;
+		totaltime+=p[i].bt;
+		twait+=p[i].wt;
+		p[i].tt=p[i].bt+p[i].wt;
+		}
+	printf("------------------------------------------\n");
+	printf(" Process\t BT \t WT \t TT\n");
+	for(int i=0;i<n;i++){
+		printf(" %d\t\t%d \t %d \t %d\n",p[i].pid,p[i].bt,p[i].wt,p[i].tt);
+		}
+	printf("------------------------------------------\n");
+	twait=twait/n;
+	totaltime=totaltime/n;
+	printf("Average Wait time:%d",twait);
+	printf("Average TurnAround time:%d",totaltime);
 	}
-	printf("%d\n",t);*/
-	sortPid(p,n);
-	display(p,n);
-}
+	
