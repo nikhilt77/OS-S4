661c444fa864a32b956314009fb33dc932f6611f
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-06-03 01:18:42 +0530

    Update FCFS_AT.c

--- Cycle-3/FCFS_AT.c
+++ Cycle-3/FCFS_AT.c
@@ -1,60 +1,52 @@
 #include<stdio.h>
+#define max 20
+struct fcfs{
+    int pid;
+    int BT;
+    int WT;
+    int AT;
+    int TAT;
+}
+p[max];
 int main(){
-
-	int bt[10]={0},at[10]={0},tat[10]={0},wt[10]={0},ct[10]={0};
-	int n,sum=0;
-	float totalTAT=0,totalWT=0;
-
-	printf("Enter number of processes	");
-	scanf("%d",&n);
-
-	printf("Enter arrival time and burst time for each process\n\n");
-
-	for(int i=0;i<n;i++)
-	{
-
-		printf("Arrival time of process[%d]	",i+1);
-		scanf("%d",&at[i]);
-		
-		printf("Burst time of process[%d]	",i+1);
-		scanf("%d",&bt[i]);
-		
-		printf("\n");
-	}
-		
-	//calculate completion time of processes 	
-
-	for(int j=0;j<n;j++)
-	{
-		sum+=bt[j];
-		ct[j]+=sum;
-	}
-
-	//calculate turnaround time and waiting times 
-
-	for(int k=0;k<n;k++)
-	{
-		tat[k]=ct[k]-at[k];
-		totalTAT+=tat[k];
-	}
-
-	
-	for(int k=0;k<n;k++)
-	{
-		wt[k]=tat[k]-bt[k];
-		totalWT+=wt[k];
-	}
-	
-	printf("Solution: \n\n");
-	printf("P#\t AT\t BT\t CT\t TAT\t WT\t\n\n");
-	
-	for(int i=0;i<n;i++)
-	{
-		printf("P%d\t %d\t %d\t %d\t %d\t %d\n",i+1,at[i],bt[i],ct[i],tat[i],wt[i]);
-	}
-		
-	printf("\n\nAverage Turnaround Time = %f\n",totalTAT/n);
-	printf("Average WT = %f\n\n",totalWT/n);
-	
-	return 0;
+    int totTAT=0,totWT=0,n;
+    printf("Enter no. of processes:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        printf("Burst time of P%d:",i);
+        scanf("%d",&p[i].BT);
+        printf("Arrival time of P%d:",i);
+        scanf("%d",&p[i].AT);
+        p[i].pid=i;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(p[i].AT>p[j].AT){
+                struct fcfs temp=p[i];
+                p[i]=p[j];
+                p[j]=temp;
+            }
+        }
+    }
+    p[0].WT=0;
+    p[0].TAT=p[0].BT;
+    totTAT+=p[0].WT;
+    totTAT+=p[0].TAT;
+    for(int i=1;i<n;i++){
+        int elapse=p[i-1].WT+p[i-1].BT+p[i-1].AT;
+        p[i].WT=(elapse>p[i].AT)?elapse-p[i].AT:0;
+        p[i].TAT=p[i].WT+p[i].BT;
+        totTAT+=p[i].TAT;
+        totWT+=p[i].WT;
+    }
+    float avgTAT=(float)totTAT/n;
+    float avgWT=(float)totWT/n;
+    printf("==================Gantt Chart==================\n");
+    printf("    Process     BT       AT         WT        TAT     \n");
+    for(int i=0;i<n;i++){
+        printf("    %d          %d         %d         %d          %d      \n",p[i].pid,p[i].BT,p[i].AT,p[i].WT,p[i].TAT);
+    }
+    printf("Average WT:%.2f\n",avgWT);
+    printf("Average TAT:%.2f\n",avgTAT);
+    return 0;
 }
