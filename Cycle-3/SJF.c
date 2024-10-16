b73f4c0b2a809b75137cd26e6588c66285730a59
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-10-16 22:56:36 +0530

    Update SJF.c

--- Cycle-3/SJF.c
+++ Cycle-3/SJF.c
@@ -1,61 +1,54 @@
 #include<stdio.h>
 #define max 20
+
 struct sjf{
-     int AT;
-     int BT;
-     int CT;
-     int TAT;
-     int WT;
-     int pid;
+    int AT;
+    int TAT;
+    int WT;
+    int BT;
+    int CT;
+    int pid;
 }p[max];
 
 int main(){
-    int n,completed=0,totWT=0,totTAT=0;
+    int n,totTAT=0,totWT=0,time=0,completed=0;
     printf("Enter no. of processes:");
     scanf("%d",&n);
     for(int i=0;i<n;i++){
-        printf("Burst time of P%d:",i);
-        scanf("%d",&p[i].BT);
-        printf("Arrival time of P%d:",i);
-        scanf("%d",&p[i].AT);
+        printf("BT AT P%d:",i);
+        scanf("%d %d",&p[i].BT,&p[i].AT);
         p[i].pid=i;
+        p[i].CT=0;
     }
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(p[j].AT>p[j+1].AT){
-                struct sjf temp=p[j];
-                p[j]=p[j+1];
-                p[j+1]=temp;
-            }
-        }
-    }
-    int time=0;
     while(completed<n){
-        int minbt=__INT_MAX__,pos=-1;
+        int min=-1;
+
         for(int i=0;i<n;i++){
-            if(p[i].BT<minbt&&p[i].AT<=time&&p[i].CT==0){
-                minbt=p[i].BT;
-                pos=i;
+            if(p[i].CT==0&&p[i].AT<=time){
+                if(p[i].BT<p[min].BT||min==-1){
+                    min=i;
+                }
             }
         }
-        if(pos==-1){
+        if(min==-1){
             time++;
         }
         else{
-            p[pos].CT=time+p[pos].BT;
-            p[pos].TAT=p[pos].CT-p[pos].AT;
-            p[pos].WT=p[pos].TAT-p[pos].BT;
-            time=p[pos].CT;
-            totTAT+=p[pos].TAT;
-            totWT+=p[pos].BT;
+           p[min].CT=time+p[min].BT;
+           p[min].TAT=p[min].CT-p[min].AT;
+           p[min].WT=p[min].TAT-p[min].BT;
+           totTAT+=p[min].TAT;
+           totWT+=p[min].WT;
+           time=p[min].CT;
+           completed++;
         }
     }
-    float avgWT=(float)totWT/n;
     float avgTAT=(float)totTAT/n;
-    printf("Process\tBT\tAT\tWT\tTAT\n");
+    float avgWT=(float)totWT/n;
+    printf("P\tBT\tAT\tTAT\tWT\n");
     for(int i=0;i<n;i++){
-        printf("%d\t%d\t%d\t%d\t%d\n",p[i].pid,p[i].BT,p[i].AT,p[i].WT,p[i].TAT);
+        printf("%d\t%d\t%d\t%d\t%d\n",p[i].pid,p[i].BT,p[i].AT,p[i].TAT,p[i].WT);
     }
-    printf("Avg TAT:%.2f",avgTAT);
-    printf("Avg WT:%.2f",avgWT);
+    printf("AvgTAT:%.2f",avgTAT);
+    printf("AvgWT:%.2f",avgWT);
 }
